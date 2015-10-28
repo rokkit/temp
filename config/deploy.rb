083a1248e8701d34d0d8e3d2afed02f9d23e8544
Maxim Indykov <indmaksim@gmail.com>
2015-10-28 17:25:48 +0300

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-set :rails_env, 'development'
+set :rails_env, 'production'
 set :domain, '192.168.1.39'
 set :term_mode, nil
 set :branch, 'master'
