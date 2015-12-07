b61d48f48a9b22edaaaf7ab254fb6020fd148b11
Maxim Indykov <indmaksim@gmail.com>
2015-12-07 19:44:27 +0300

    fix

--- app/controllers/api/v1/achievements_controller.rb
+++ app/controllers/api/v1/achievements_controller.rb
@@ -6,7 +6,12 @@ class Api::V1::AchievementsController < Api::V1::BaseController
   # Если какое-либо достижение открыто у юзера,
   # то оно отмечается значением в ответе
   def index
-    @achievements = Achievement.all.order(:id)
+
+    if params[:role] == 'hookmaster'
+      @achievements = Achievement.where(role: 1).order(:id)
+    else
+      @achievements = Achievement.where(role: 0).order(:id)
+    end
     respond_with @achievements
   end
 end
