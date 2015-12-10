a0ab24e3226c4998250476cf7bf994bb80da4324
Maxim Indykov <indmaksim@gmail.com>
2015-12-10 12:28:56 +0300

    Enable errors

--- app/controllers/api/v1/works_controller.rb
+++ app/controllers/api/v1/works_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::WorksController < ApplicationController
   respond_to :json
+  before_action :authenticate_user!
   # Получить список столиков для указанного заведения
   def index
     @works = Work.where(user_id: current_user.id).includes(:lounge)
