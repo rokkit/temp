a6410d2d6377b654e2af2a8c026712354a71665c
Maxim Indykov <indmaksim@gmail.com>
2015-12-11 17:14:47 +0300

    Add sorting

--- app/services/soap_service.rb
+++ app/services/soap_service.rb
@@ -1,8 +1,11 @@
 class SoapService
   def self.call(method, params)
+    begin
     client = Savon.client(wsdl: 'http://176.112.198.251/uhp_demo1/ws/obmen.1cws?wsdl')
     response = client.call(method, params)
     puts response.body.inspect
+    rescue Savon::HTTPError
+    end
     true
   end
 end
