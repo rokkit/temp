974a760b47afb46300d892182f71fa480f92a612
Maxim Indykov <indmaksim@gmail.com>
2015-12-15 16:45:57 +0300

    Add meets

--- app/services/sms_service.rb
+++ app/services/sms_service.rb
@@ -2,15 +2,15 @@ class SMSService
   # Отправить смс на указанный номер, используя сторонний СМС сервис
   # TODO: Пока заглушка
   def self.send(phone, text)
-    if !Rails.env.test?
-      request = Net::HTTP.post_form URI.parse('http://smsc.ru/sys/send.php'),
-       {
-         'login' => 'uhp',
-         'psw' => 'uhpfamily',
-         'phones' => "#{phone}",
-         'mes' => text,
-         'charset' => 'utf-8'
-       }
-     end
+    # if !Rails.env.test?
+    #   request = Net::HTTP.post_form URI.parse('http://smsc.ru/sys/send.php'),
+    #    {
+    #      'login' => 'uhp',
+    #      'psw' => 'uhpfamily',
+    #      'phones' => "#{phone}",
+    #      'mes' => text,
+    #      'charset' => 'utf-8'
+    #    }
+    #  end
   end
 end
