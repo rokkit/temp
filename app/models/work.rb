d6f5dc8c3ce38a54fd3db0a0f6b51ca2c4b8e642
Maxim Indykov <indmaksim@gmail.com>
2015-12-17 15:27:28 +0300

    Add users hook exp

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,4 +1,11 @@
 class Work < ActiveRecord::Base
   belongs_to :lounge
   belongs_to :user
+
+  after_initialize :init
+
+  def init
+    self.work_at = Time.zone.now if self.work_at.nil?
+    self.end_work_at = Time.zone.now + 8.hours if self.work_at.nil?
+  end
 end
