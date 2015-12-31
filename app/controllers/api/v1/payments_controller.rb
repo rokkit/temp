493d05c2f69f02cd32832e7cef74171dec0862dd
Maxim Indykov <indmaksim@gmail.com>
2015-12-31 13:19:23 +0300

    fix

--- app/controllers/api/v1/payments_controller.rb
+++ app/controllers/api/v1/payments_controller.rb
@@ -5,19 +5,34 @@ class Api::V1::PaymentsController < Api::V1::BaseController
   # TODO: Пока непонятно какие данные кроме суммы мы можем получить от 1С
   def create
     user = User.find_by_phone(params[:phone])
-    params[:amount] ||= 100
+    params[:amount] ||= 100.0
     if user
-      payment = Payment.new(user: user, amount: params[:amount])
-      if params[:reservation_code].present?
-        reservation = Reservation.where(code: params[:reservation_code]).first
-        if reservation
-          payment.reservation = reservation
+
+      payments_ext = user.get_payments_from_ext()
+      total_amount = payments_ext.map { |p| p['_Fld1574'] }.reduce(0) { |a, sum| sum += a }.to_f
+
+      if total_amount > 0 && total_amount > user.experience
+        amount_diff = total_amount - user.experience
+        payment = Payment.new(user: user, amount: params[:amount])
+        if params[:reservation_code].present?
+          reservation = Reservation.where(code: params[:reservation_code]).first
+          if reservation
+            payment.reservation = reservation
+          end
         end
-      end
-      if payment.save
-        head :ok
+        if payment.save
+          head :ok
+          return
+        else
+          render json: {error: 'wrong_payment_data'}
+          return
+        end
+      else
+        render json: {error: 'zero_payments'}
         return
       end
+
+
     else
       render json: {error: 'user not found'}
     end
